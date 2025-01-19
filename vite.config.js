f92ebb1d34dc092309b8ac8f88abcb2eaaa9b2b8
Lena <elenabelichenko9@gmail.com>
2025-01-19 02:42:00 +0200

    start-1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/my-react-project-3/",
+  base: "/my-react-project-4/",
   plugins: [react()],
   build: {
     sourcemap: true,
